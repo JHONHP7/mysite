97fb4adbf37c6533d50cdc15ff62f5aefea219e6
jhonatan win casa <jhonatansilva@id.uff.br>
2023-11-20 22:43:43 -0300

    Admin Dashboard

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import ToDoList, Item
 # Register your models here.
+admin.site.register(ToDoList) # aqui vc registra os modelos para que eles apareçam no admin do django
+admin.site.register(Item) # aqui vc registra os modelos para que eles apareçam no admin do django
